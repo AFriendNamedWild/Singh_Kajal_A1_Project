c54e87df9ed139b5cba04e83b5de06ca53c3f258
EH <EH@WILDHARE>
2024-09-25 19:55:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+
+Console.WriteLine("Welcome to Russian Roulette...");
+Console.WriteLine("Choose a letter to see if there's a bullet or not");
+Console.WriteLine("Pray you survive friend...");
+
+bool A = true;
+bool B = true;
+bool C = false;
+bool D = true;
+bool E = false; 
+
+
+Console.WriteLine(A);
+Console.WriteLine(B);
+Console.WriteLine(C);
+Console.WriteLine(D);
+Console.WriteLine(E);
+
+
+A = bool.Parse(Console.ReadLine());
+Console.WriteLine("R.I.P");
+B = bool.Parse(Console.ReadLine());
+Console.WriteLine("R.I.P");
+C = bool.Parse(Console.ReadLine());
+Console.WriteLine("Safe");
+D = bool.Parse(Console.ReadLine());
+Console.WriteLine("R.I.P");
+E = bool.Parse(Console.ReadLine());
+Console.WriteLine("Safe");
+
+if (A = true);
